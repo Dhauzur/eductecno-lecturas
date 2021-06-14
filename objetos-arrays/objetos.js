53f8b81cf1eaa016bef24cfa32fa88509be8ad68
Mauro Perez <mmauroperez@gmail.com>
2021-06-14 12:22:41 -0400

    error asincrono

---
+++ objetos-arrays/objetos.js
@@ -0,0 +1,16 @@
+let perros = [
+  {
+    raza: "Pastor Alemán",
+    origen: "Alemania",
+    pelaje: "Lanudo",
+    peso: "33kg",
+    edad: 12
+  },
+  {
+    raza: "Poodle",
+    origen: "Francia",
+    pelaje: "Lanudo",
+    peso: "20kg",
+    edad: 14
+  }
+];
