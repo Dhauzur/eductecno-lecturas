a00598365504f59fd71cce9bda378221303366f3
Mauro Perez <mmauroperez@gmail.com>
2021-07-13 10:40:45 -0400

    computed properties y styles binding

--- vuestart/src/App.vue
+++ vuestart/src/App.vue
@@ -1,5 +1,6 @@
 <template lang="html">
   <div id="app">
+    <h2>Cantidad de frutas : {{ cantidadDeFrutas }}</h2>
     <button @click="mostrarComponente">Mostrar u ocultar</button>
     <div v-if="mostrar">
       {{ message }}
@@ -12,8 +13,13 @@
       <input type="checkbox" id="checkbox" v-model="checked" />
       {{ checked }}
       <ul>
-        <li v-for="fruta in frutas">
-          <span>{{ fruta.name }}</span>
+        <li v-for="(fruta, index) in frutas">
+          <span
+            :style="[frutaUnica, borderFruta]"
+            :class="{ rayado: fruta.comprado && checked }"
+            @click="eliminarFruta(index)"
+            >{{ fruta.name }}</span
+          >
         </li>
       </ul>
     </div>
@@ -30,18 +36,59 @@ export default {
   },
   data() {
     return {
+      frutaUnica: {
+        "font-size": "12px",
+        color: "red !important"
+      },
+      borderFruta: {
+        "border-bottom": "2px solid black"
+      },
       checked: true,
-      frutas: [{ name: "Frutillas" }, { name: "Manzanas" }, { name: "Kiwis" }],
+      frutas: [
+        { name: "Frutillas", comprado: true },
+        { name: "Manzanas", comprado: true },
+        { name: "Kiwis", comprado: false }
+      ],
       mostrar: true,
       message: "Desafio  Latam!!! 🤟🤟",
       pathImage:
         "https://assets.pokemon.com/assets/cms2/img/pokedex/full/131.png"
     };
   },
+  computed: {
+    cantidadDeFrutas() {
+      if (this.checked)
+        return (
+          "Desactive funcion checked, " +
+          this.frutas[this.frutas.length - 1].name
+        );
+      return this.frutas.length;
+    }
+  },
+  beforeCreate() {
+    console.log("before Create : ", this.message);
+  },
+  created() {
+    console.log(" Created : ", this.message);
+  },
+  beforeMount() {
+    console.log("before mount", this.message);
+  },
+  mounted() {
+    console.log("mounted", this.message);
+  },
+  beforeUpdate() {
+    console.log("before Update", this.message);
+  },
+  updated() {
+    console.log("Updated", this.message);
+  },
   methods: {
+    eliminarFruta() {},
     metodoDelPadre() {
       console.log("Evento del hijo controlado");
       this.checked = !this.checked;
+      this.frutas.push({ name: "NuevaFruta", comprado: true });
     },
     mostrarComponente() {
       console.log("Soy un metodo");
@@ -50,3 +97,9 @@ export default {
   }
 };
 </script>
+
+<style>
+.rayado {
+  text-decoration: line-through;
+}
+</style>
